a895d0a52874a617126bf211ebc57d7b1282ef31
gaojiankang <gaojiankang@thunisoft.com>
2022-04-22 14:58:00 +0800

    【+】新增

---
+++ gjk-demo/src/main/java/com/gjk/spring_learn/Student.java
@@ -0,0 +1,44 @@
+package com.gjk.spring_learn;
+
+/**
+ * Student
+ *
+ * @author: GJK
+ * @date: 2022/4/22 10:28
+ * @description:
+ */
+public class Student {
+
+
+
+	private int id;
+
+	private String username;
+
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	@Override
+	public String toString() {
+		return "Student{" +
+				"id=" + id +
+				", username='" + username + '\'' +
+				'}';
+	}
+
+
+}
